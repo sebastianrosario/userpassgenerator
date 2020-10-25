3bc232c2e563afb1f4d35aee3bcf270e1d641816
Sebastian Rosario <sebastian6417@gmail.com>
2020-10-25 16:07:07 -0400

    Add files via upload

--- addusercommands.py
+++ addusercommands.py
@@ -2,6 +2,7 @@ import subprocess
 import shlex
 
 bash_shell = '/bin/bash'
+cshell = '/bin/csh'
 user_arr = []
 
 
@@ -25,6 +26,16 @@ def get_usr_id():
     return last_userid
 
 
+def define_password(usrname, pword):
+    echo_command_args = shlex.split("echo -e '{}\n{}\n'".format(pword, pword))
+    password_command_args = shlex.split('passwd {}'.format(usrname))
+
+    echo_command = subprocess.Popen(echo_command_args, stdout=subprocess.PIPE)
+    password_command = subprocess.Popen(password_command_args, stdin=echo_command.stdout, stdout=subprocess.PIPE)
+    echo_command.stdout.close()
+    password_command.communicate()
+
+
 def admin_add(groupname):
     admin_skel = '/etc/skel/adminskel'
     get_usrs(groupname)
@@ -32,7 +43,24 @@ def admin_add(groupname):
 
     for combo in range(len(user_arr)):
         password = user_arr[combo][1]
-        adduser_command_args = shlex.split('useradd -m -d /home/admin/users/{} -u {} -G {} -k {} -s {} {}'.format(user_arr[combo][0], userid, groupname, admin_skel, bash_shell, user_arr[combo][0]))
+        username = user_arr[combo][0]
+
+        adduser_command_args = shlex.split('useradd -m -d /home/admin/users/{} -u {} -G {} -k {} -s {} {}'.format(user_arr[combo][0], userid, groupname, admin_skel, bash_shell, username))
+        subprocess.call(adduser_command_args)
+
+        define_password(username, password)
+
+        userid += 1
+
+
+def staff_add(groupname):
+    staff_skel = '/etc/skel/staffskel'
+    get_usrs(groupname)
+    userid = int(get_usr_id()) + 1
+
+    for combo in range(len(user_arr)):
+        password = user_arr[combo][1]
+        adduser_command_args = shlex.split('useradd -m -d /home/staff/users/{} -u {} -G {} -k {} -s {} {}'.format(user_arr[combo][0], userid, groupname, staff_skel, bash_shell, user_arr[combo][0]))
         echo_command_args = shlex.split("echo -e '{}\n{}\n'".format(password, password))
         password_command_args = shlex.split('passwd {}'.format(user_arr[combo][0]))
 
@@ -44,24 +72,17 @@ def admin_add(groupname):
         userid += 1
 
 
-def staff_add(groupname):
-    staff_skel = '/etc/skel/staffskel'
-    get_usrs(groupname)
-    userid = int(get_usr_id()) + 1
-
-    for combo in range(len(user_arr)):
-        adduser_command_args = shlex.split('useradd -m -d /home/staff/users/{} -u {} -G {} -k {} -s {} {}'.format(user_arr[combo][0], userid, groupname, staff_skel, bash_shell, user_arr[combo][0]))
-        subprocess.call(adduser_command_args)
-        userid += 1
-
-
 def developer_add(groupname):
     developer_skel = '/etc/skel/developerskel'
-    cshell = '/bin/csh'
     get_usrs(groupname)
     userid = int(get_usr_id()) + 1
+
     for combo in range(len(user_arr)):
+        password = user_arr[combo][1]
         adduser_command_args = shlex.split('useradd -m -d /home/developer/users/{} -u {} -s {} -G {} -k {} {}'.format(user_arr[combo][0], userid, cshell, groupname, developer_skel, user_arr[combo][0]))
+        echo_command_args = shlex.split("echo -e '{}\n{}\n'".format(password, password))
+        password_command_args = shlex.split('passwd {}'.format(user_arr[combo][0]))
+
         subprocess.call(adduser_command_args)
         print(adduser_command_args)
         userid += 1
